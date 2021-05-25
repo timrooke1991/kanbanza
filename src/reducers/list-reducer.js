612b98b0461674bbfbfcacf3d0e3f24c696bd780
Tim Rooke <timrooke1991@gmail.com>
2021-05-25 17:32:52 +0100

    Card reducer and UI in place

--- src/reducers/list-reducer.js
+++ src/reducers/list-reducer.js
@@ -1,6 +1,22 @@
 import { lists as defaultLists } from '../normalized-state';
 
 const listsReducer = (lists = defaultLists, action) => {
+  if (action.type === 'CARD_CREATE') {
+    const { cardId, listId } = action.payload;
+    const entities = {
+      ...lists.entities
+    };
+
+    entities[listId] = {
+      ...entities[listId],
+      cards: entities[listId].cards.concat(cardId)
+    };
+
+    return {
+      ...lists,
+      entities
+    };
+  }
   return lists;
 };
 
